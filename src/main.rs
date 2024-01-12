79fa83dca69723984288f5bef59999288368c023
Masterism88 <nate.j.dufresne@gmail.com>
2024-01-11 19:27:20 -0500

    Separated linux and windows code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use owo_colors::OwoColorize;
+use serde::{Deserialize, Serialize};
 use std::fs;
+use std::fs::File;
 use std::io;
+use std::io::Write;
 use std::net::TcpStream;
 use std::path;
 
@@ -17,35 +21,56 @@ const LOGO: &str = r#"
            Based off of the the original by Devn00b https://www.eq2emu.com
     "#;
 
+#[derive(Serialize, Deserialize)]
+struct Updates {
+    sqlupdate: i32,
+    exeupdate: i32,
+    luaupdate: i32,
+    mapupdate: i32,
+    sendbugs: i32,
+    lsupdate: i32,
+    updatels: i32,
+    dbeditor: i32,
+}
 fn main() {
     let server_name: &str = r#"Zeklabs.com"#;
-    let file_path = path::Path::new("settings.txt");
-    const CONFIG_FILE: &str = "/settings.";
+    let file_path = path::Path::new("config.json");
 
     println!("{}", LOGO);
 
     println!("Server Name: {}", server_name);
 
-    //Check if config file exists, if not create it
+    // Check type of OS
+    match std::env::consts::OS {
+        "linux" => linux(),
+        "windows" => windows(),
+        _ => println!("Running on an unknown operating system"),
+    }
+
+    let update = Updates {
+        sqlupdate: 1,
+        exeupdate: 1,
+        luaupdate: 1,
+        mapupdate: 1,
+        sendbugs: 1,
+        lsupdate: 1,
+        updatels: 1,
+        dbeditor: 1,
+    };
+    // Serialize the data to a JSON string
+    let json_data = serde_json::to_string(&update).expect("Failed to serialize to JSON");
+
+    // Check if config file exists, if not create it
     if file_path.exists() {
-        println!("File exists");
+        println!("Config file exists");
+        fs::write(file_path, json_data.as_bytes()).expect("Unable to write file");
     } else {
         // Create the file using `fs::File::create()`
         match fs::File::create(&file_path) {
-            Ok(_) => println!("File created successfully"),
+            Ok(_) => println!("Config file created successfully"),
             Err(e) => println!("Error creating file: {}", e),
         }
-    }
-
-    // Check for internet access
-    const ZEKLABS: &str = r#"Zeklabs.com"#;
-
-    let reachable = is_reachable(ZEKLABS);
-
-    if reachable {
-        println!("{} is reachable", ZEKLABS);
-    } else {
-        println!("{} is not reachable", ZEKLABS);
+        fs::write(file_path, json_data.as_bytes()).expect("Unable to write file");
     }
 }
 
@@ -55,4 +80,39 @@ fn is_reachable(domain_name: &str) -> bool {
         Ok(_) => true,   // Connection successful
         Err(_) => false, // Connection failed
     }
+}
+
+fn linux() {
+    println!("Running on Linux");
+
+    // Check for internet access
+    const ZEKLABS: &str = r#"Zeklabs.com"#;
+    let reachable: bool = is_reachable(ZEKLABS);
+
+    if reachable {
+        println!("{}", format!("{} is reachable", ZEKLABS).green());
+    } else {
+        println!("{}", format!("{} is not reachable", ZEKLABS).red());
+    }
+}
+
+fn windows() {
+    println!("Running on Windows");
+
+    // Check for internet access
+    const ZEKLABS: &str = r#"Zeklabs.com"#;
+    let reachable: bool = is_reachable(ZEKLABS);
+
+    if reachable {
+        println!("{} is not reachable", ZEKLABS);
+    } else {
+        println!("{} is not reachable", ZEKLABS);
+    }
+
+    // Ask the user for input before exiting
+    println!("Press Enter to exit...");
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line");
 }
