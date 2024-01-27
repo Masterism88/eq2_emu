3836319834215fe39e53f4c6af82c8b53018d8fd
Masterism88 <nate.j.dufresne@gmail.com>
2024-01-27 17:03:20 -0500

    Added redist for windows

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use download_rs::async_download::Download;
 use owo_colors::OwoColorize;
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::Write;
 use std::net::TcpStream;
-use std::path;
+use std::path::Path;
+use std::path::{self, PathBuf};
+use std::process::Command;
 
 const LOGO: &str = r#"
 
@@ -17,12 +21,13 @@ const LOGO: &str = r#"
     88         Y8,    "88,,8P   a8P'        88           88   `8b d8'   88  88       88  
     88          Y8a.    Y88P   d8"          88           88    `888'    88  "8a,   ,a88  
     88888888888  `"Y8888Y"Y8a  88888888888  88888888888  88     `8'     88   `"YbbdP'Y8  
-       Windows Installer Rust version by Masterism 
-           Based off of the the original by Devn00b https://www.eq2emu.com
+       EQ2EMu Installer Rust version by Masterism 
+           Based off of the original by Devn00b https://www.eq2emu.com https://discord.gg/5Cavm9NYQf
     "#;
 
 #[derive(Serialize, Deserialize)]
 struct Updates {
+    firstrun: i32,
     sqlupdate: i32,
     exeupdate: i32,
     luaupdate: i32,
@@ -34,7 +39,6 @@ struct Updates {
 }
 fn main() {
     let server_name: &str = r#"Zeklabs.com"#;
-    let file_path = path::Path::new("config.json");
 
     println!("{}", LOGO);
 
@@ -48,6 +52,7 @@ fn main() {
     }
 
     let update = Updates {
+        firstrun: 1,
         sqlupdate: 1,
         exeupdate: 1,
         luaupdate: 1,
@@ -61,6 +66,8 @@ fn main() {
     let json_data = serde_json::to_string(&update).expect("Failed to serialize to JSON");
 
     // Check if config file exists, if not create it
+    let file_path = path::Path::new("config.json");
+    println!("Config File");
     if file_path.exists() {
         println!("Config file exists");
         fs::write(file_path, json_data.as_bytes()).expect("Unable to write file");
@@ -72,6 +79,17 @@ fn main() {
         }
         fs::write(file_path, json_data.as_bytes()).expect("Unable to write file");
     }
+    println!("Downloading SQL Full Update");
+    download("https://zeklabs.com/dl/eq2emudb.rar", "eq2emudb.rar");
+
+    extract("eq2emudb.rar");
+
+    // Ask the user for input before exiting
+    println!("Press Enter to exit...");
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line");
 }
 
 fn is_reachable(domain_name: &str) -> bool {
@@ -83,7 +101,7 @@ fn is_reachable(domain_name: &str) -> bool {
 }
 
 fn linux() {
-    println!("Running on Linux");
+    println!("Linux Detected");
 
     // Check for internet access
     const ZEKLABS: &str = r#"Zeklabs.com"#;
@@ -97,22 +115,87 @@ fn linux() {
 }
 
 fn windows() {
-    println!("Running on Windows");
+    println!("Windows Detected");
 
     // Check for internet access
     const ZEKLABS: &str = r#"Zeklabs.com"#;
     let reachable: bool = is_reachable(ZEKLABS);
 
     if reachable {
-        println!("{} is not reachable", ZEKLABS);
+        println!("{} is reachable", ZEKLABS);
+
+        // Detect if OS is 32bit or 64 bit
+        println!("Detecting if OS is 32bit or 64 bit");
+
+        let target_pointer_width = if cfg!(target_pointer_width = "32") {
+            println!("32 bit OS detected");
+            "32"
+        } else {
+            println!("64 bit OS detected");
+            "64"
+        }
+        .to_string();
+
+        //Get current working directory
+        let cwd = std::env::current_dir().expect("Failed to get current working directory");
+        println!("{}", cwd.to_string_lossy());
+
+        let full_filename = format!("vc_redist.x{}.exe", target_pointer_width,);
+
+        println!("{}", full_filename);
+        let url_prefix = "https://aka.ms/vs/17/release/";
+        let redist_url = format!("{}{}", url_prefix, full_filename);
+        println!("redist_url");
+        println!("{}", redist_url);
+
+        println!("Downloading Microsoft Visual C++ Redistributable");
+
+        download(&redist_url, &full_filename);
+        install_redist(&PathBuf::from(&full_filename));
     } else {
         println!("{} is not reachable", ZEKLABS);
     }
+}
+fn install_redist(exe_path: &PathBuf) {
+    println!("installing {}, please wait", exe_path.to_string_lossy());
+    let status = Command::new(exe_path)
+        //.arg("/q")
+        //.arg("/norestart")
+        .status()
+        .expect("Failed to execute command");
 
-    // Ask the user for input before exiting
-    println!("Press Enter to exit...");
-    let mut buffer = String::new();
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("Failed to read line");
+    if status.success() {
+        println!("Executable ran successfully");
+    } else {
+        eprintln!("Executable failed with exit code: {:?}", status.code());
+    }
+}
+
+fn extract(filename: &str) {
+    let status = Command::new("unrar")
+        .arg("x")
+        .arg("-y")
+        .arg("-inul")
+        .arg(filename)
+        .status()
+        .expect("Failed to execute command");
+
+    println!("extracting {}, please wait", filename);
+
+    if status.success() {
+        println!("Executable ran successfully");
+    } else {
+        eprintln!("Executable failed with exit code: {:?}", status.code());
+    }
+}
+
+fn download(url: &str, filename: &str) {
+    /* let url = &redist_url;
+    let filename = &full_filename; */
+    let download = Download::new(url, Some(filename), None);
+
+    match download.download() {
+        Ok(_) => println!("Download Complete"),
+        Err(e) => println!("Download error: {}", e.to_string()),
+    }
 }
